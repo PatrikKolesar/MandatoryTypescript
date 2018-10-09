c6904f5afdae7bef269717cb36ea96fcfe96471e
Patrik Kolesar <patr273e@edu.easj.dk>
2018-10-09 10:07:41 +0200

    Finished

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,24 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("input");
+let outputElement: HTMLOutputElement = <HTMLOutputElement> document.getElementById("output");
+
+function toOunces():void{
+
+    let input:string = inputElement.value;  
+    let output:number = parseFloat(input)/28.34952;
+    outputElement.innerHTML = output.toString();
+
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+function toGrams():void{
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+    let input:string = inputElement.value;  
+    let output:number = parseFloat(input)*28.34952;
+    outputElement.innerHTML = output.toString();
+
+}
+
+let button1: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button1");
+let button2: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button2");
+
+button1.addEventListener("click",toOunces);
+button2.addEventListener("click",toGrams);
